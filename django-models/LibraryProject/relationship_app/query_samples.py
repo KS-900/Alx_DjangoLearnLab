7885d36207f9ada51e1d1c7e2a6d8bd56da763fc
Karabo Satekge <kb2.satekge@gmail.com>
2025-02-20 23:03:29 +0200

    Task 0 attempt 3

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,14 +2,14 @@ from .models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
     try:
-        author = Author.objects.get(name = author_name)
+        author = Author.objects.get(name=author_name)
         return Author.objects.filter(author=author)
     except Author.DoesNotExist:
         return "Author not found"
 
 def get_all_books_in_library(library_name):
     try:
-        Library.objects.get(name= library_name)
+        Library.objects.get(name=library_name)
         return Library.books.all()
     except Library.DoesNotExist:
         return "Library not found"
