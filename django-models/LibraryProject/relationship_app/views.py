e4783af09f0526966bc33a5435ed2e3e3491447d
Karabo Satekge <kb2.satekge@gmail.com>
2025-03-02 12:10:40 +0200

    Last week attempt 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect 
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
-from .models import Book, Library
+from .models import Book 
+from .models import Library
 from django.views.generic import DetailView
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth import login
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
-relationship_app/list_books.html, Book.objects.all()
+relationship_app/list_books.html, relationship_app/library_detail.html, Book.objects.all()
 def list_books(request):
     books =  Book.objects.all()
     response_text = "\n".join([f"{book.title} by {book.author.name}" for book in books])
@@ -33,6 +36,16 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('admin_view')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 @user_passes_test(is_admin)
