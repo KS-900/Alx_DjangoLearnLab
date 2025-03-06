a036d24d98cb496f421b571f997cc3219655660d
Karabo Satekge <kb2.satekge@gmail.com>
2025-03-06 12:28:53 +0200

    Managing Permissions and Groups in Django(3)

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -6,6 +6,8 @@ from .models import Book
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     return "pemission granted"
+def book_list(request):
+    return Book.objects.all()
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request):
     return "permission granted"
