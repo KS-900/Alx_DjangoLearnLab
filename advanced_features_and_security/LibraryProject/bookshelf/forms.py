ca9b54c5c04e7aec102727a849805f417668eb55
Karabo Satekge <kb2.satekge@gmail.com>
2025-03-06 20:56:31 +0200

    Implementing Security Best Practices in Django(2)

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import Group
 from django.forms import ModelForm
 
+ExampleForm
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Group
