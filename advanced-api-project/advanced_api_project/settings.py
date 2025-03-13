9d4a9f9bac630cb7c5f75ba598e7e06938a0286d
Karabo Satekge <kb2.satekge@gmail.com>
2025-03-13 09:25:00 +0200

    Task 1

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -70,6 +70,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'advanced_api_project.wsgi.application'
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS':
+['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 
 # Database
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 
-# Create your views here.
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
