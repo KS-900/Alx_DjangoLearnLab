2fe83737c7fa7c4f7b8b760a35a4f0c277ec13ed
Karabo Satekge <kb2.satekge@gmail.com>
2025-03-13 09:47:31 +0200

    Task 3

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import BookListView
+from .views import BookDetailView
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
+    path('book/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,10 +7,20 @@ from rest_framework import filters
 from rest_framework.filters import SearchFilter
 from rest_framework.filters import OrderingFilter
 from django_filters import rest_framework
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-class BookListView(generics.ListAPIView):
+class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
