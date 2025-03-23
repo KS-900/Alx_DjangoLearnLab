4fba08cf4ae22da8bd108126065ec383524e6bd3
Karabo Satekge <kb2.satekge@gmail.com>
2025-03-23 17:47:19 +0200

    Task3(1)

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -15,3 +15,4 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
